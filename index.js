fb469a5640e2f1559a8b4a24f363d129f8f5fa33
root <help@ubg100.net>
2024-02-14 16:59:21 +0100

    update

---
+++ index.js
@@ -0,0 +1,9 @@
+loadLib("libs/babel-require.js"),
+// loadLib("https://h5gamessdk.yyggames.com/sdk/laya/2.8.0/laya.core.js"), 
+// loadLib("https://h5gamessdk.yyggames.com/sdk/laya/2.8.0/laya.ui.js"), 
+// loadLib("https://h5gamessdk.yyggames.com/sdk/laya/2.8.0/laya.d3.js"), 
+    loadLib("libs/laya.core.js"),
+    // loadLib("libs/domparserinone.js"),
+    loadLib("libs/laya.ui.js"),
+    loadLib("libs/laya.d3.js"), 
+    loadLib("zsLibs/index.js");
